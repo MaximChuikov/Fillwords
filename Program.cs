de19e2fa625f05cc4cd9275e4f96761a0ab86c0c
Максим <machsim26@gmail.com>
2020-11-15 09:16:18 +0500

    выделение

--- Program.cs
+++ Program.cs
@@ -4,6 +4,21 @@ namespace Меню
 {
     class Program
     {
+        static void SelectingString(string oldStr, string newStr, int x1, int x2, int oldY, int newY)
+        {
+            Console.SetCursorPosition(x2, oldY);
+            for (int i = x2; i > x1; i--)
+            {
+                Console.Write("\b \b");
+            }
+            Console.Write(oldStr);
+
+            Console.SetCursorPosition(x1, newY);
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.Write(newStr);
+            Console.BackgroundColor = ConsoleColor.Black;
+
+        }
         static void SetCursorAndWrite(string str, int x, int y)
         {
             Console.SetCursorPosition(x, y);
@@ -43,6 +58,26 @@ namespace Меню
             for (int i = 0; i < menustrings.Length; i++)
             {
                 SetCursorAndWrite(menustrings[i], RIGHTINDENT, UPINDENT + i);
+            }
+            int selectedLine = 0;
+            SelectingString(menustrings[selectedLine], menustrings[selectedLine], RIGHTINDENT, RIGHTINDENT + WIDTH, UPINDENT + selectedLine, UPINDENT);
+            ConsoleKeyInfo key;
+            for (; ; )
+            {
+                Console.SetCursorPosition(RIGHTINDENT - 1, UPINDENT + LENGHT + 1);
+                key = Console.ReadKey();
+
+                if (key.Key == ConsoleKey.W || key.Key == ConsoleKey.UpArrow && selectedLine != 0)
+                {
+                    selectedLine--;
+                    SelectingString(menustrings[selectedLine + 1], menustrings[selectedLine], RIGHTINDENT, RIGHTINDENT + WIDTH, UPINDENT + selectedLine + 1, UPINDENT + selectedLine);
+                }
+
+                if (key.Key == ConsoleKey.S || key.Key == ConsoleKey.DownArrow && selectedLine != LENGHT - 1)
+                {
+                    selectedLine++;
+                    SelectingString(menustrings[selectedLine - 1], menustrings[selectedLine], RIGHTINDENT, RIGHTINDENT + WIDTH, UPINDENT + selectedLine - 1, UPINDENT + selectedLine);
+                }
             }
         }
     }
